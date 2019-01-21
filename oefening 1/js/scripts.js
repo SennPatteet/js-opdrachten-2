f345d900f9673b4c744cdc70a8d7f6e655db1221
Senn Pateet <senn.patteet@hotmail.com>
2019-01-21 14:33:46 +0100

    oefening 1

--- oefening 1/js/scripts.js
+++ oefening 1/js/scripts.js
@@ -1,1 +1,14 @@
 
+
+
+document.getElementById("button").onclick = function surfaceRectangle() {
+  let width = prompt("breedte?");
+  let height = prompt("hoogte?");
+  alert(`De oppervlakte is ${width*height}`)
+};
+
+document.getElementById("button2").onclick = function perimeterRectangle() {
+  let width = prompt("breedte?");
+  let height = prompt("hoogte?");
+  alert(`De omtrek is ${(width*2)+(height*2)}`)
+};
