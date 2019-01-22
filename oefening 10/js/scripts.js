4509e77084b688dd2b65f26a39b40972a2a7cb93
Senn Pateet <senn.patteet@hotmail.com>
2019-01-22 14:59:29 +0100

    oefening 10

--- oefening 10/js/scripts.js
+++ oefening 10/js/scripts.js
@@ -1,1 +1,25 @@
 
+// WERKT MA MAG NI LOL --------------------------------------------------
+
+// const sum = () => {
+//   let eersteGetal = prompt("voer een getal in");
+//   let tweedeGetal = prompt("voer NOG een getal in");
+//   let derdeGetal = prompt("voer een laatste getal in");
+//
+//   alert(`De som is: ${parseInt(eersteGetal) + parseInt(tweedeGetal) + parseInt(derdeGetal)}`);
+//
+// }
+
+const sum = () => {
+
+  let total = 0;
+
+  for (i=0; i<3; i++) {
+    let numbers = parseInt(prompt("number pls"));
+
+    total += numbers
+
+  }
+
+  alert(total);
+}
