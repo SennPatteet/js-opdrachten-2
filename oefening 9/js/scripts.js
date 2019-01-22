35b7b0d50e887ab33a105636f1072b7494d59756
Senn Pateet <senn.patteet@hotmail.com>
2019-01-22 14:08:45 +0100

    oefening 9

--- oefening 9/js/scripts.js
+++ oefening 9/js/scripts.js
@@ -1,1 +1,12 @@
 
+const testWhile = () => {
+
+  let string = prompt("Typ iets met een 'p' in pls");
+
+
+  while (string.indexOf("p") == -1 ) {
+  string =  prompt("Typ iets met een 'p' in pls");
+  };
+
+  alert(string);
+};
