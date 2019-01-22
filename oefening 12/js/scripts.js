470f6409e1bc225f4fa2e242cd2359a9dacf9a57
Senn Pateet <senn.patteet@hotmail.com>
2019-01-22 16:50:00 +0100

    alles behalve 11 (en mss 5)

--- oefening 12/js/scripts.js
+++ oefening 12/js/scripts.js
@@ -1,1 +1,52 @@
 
+const tempConversion = () => {
+
+let soort = prompt("Welke conversion?:\n \n0. End of the program\n1. Celsius to Fahrenheit\n2. Celsius to Kelvin\n3. Fahrenheit to Celsius\n4. Fahrenheit to Kelvin\n5. Kelvin to Celsius\n6. Kelvin to Fahrenheit")
+
+
+
+switch (soort) {
+
+    case "0":
+    break;
+
+    case "1":
+     getal = prompt ("welk getal wil je converten? (Celcius to Fahrenheit)")
+    alert(`${parseInt(getal)}°C = ${(parseInt(getal)*9/5)+32} °Fahrenheit`);
+    break;
+
+    case "2":
+     getal = prompt ("welk getal wil je converten? (Celcius to Kelvin)")
+    alert(`${parseInt(getal)}°C = ${parseInt(getal)+273.15} Kelvin`);
+    break;
+
+    case "3":
+     getal = prompt ("welk getal wil je converten? (Fahrenheit to Celcius)")
+    alert(`${parseInt(getal)}°Fahrenheit = ${(parseInt(getal)-32) / (9/5)}°C`);
+    break;
+
+    case "4":
+     getal = prompt ("welk getal wil je converten? (Fahrenheit to Kelvin)")
+    alert(`${parseInt(getal)}°Fahrenheit = ${((parseInt(getal)-32) * (5/9)) + 273.15} Kelvin`);
+    break;
+
+    case "5":
+     getal = prompt ("welk getal wil je converten? (Kelvin to Celcius)")
+    alert(`${parseInt(getal)} Kelvin = ${parseInt(getal) - 273.15}°C`);
+    break;
+
+    case "6":
+     getal = prompt ("welk getal wil je converten? (Kelvin to Fahrenheit)")
+    alert(`${parseInt(getal)} Kelvin = ${(parseInt(getal)* 9/5) - 459.67}°Fahrenheit`);
+    break;
+  }
+
+
+// [°F] = ([°C] x 9/5) + 32
+// [K] = [°C] + 273,15
+// [°C] = ([°F] - 32) / (9/5)
+// [K] = (([°F] - 32) x (5/9)) + 273,15
+// [°C] = [K] - 273,15
+// [°F] = ([K] x 9/5) - 459,67
+
+}
--- oefening 13/js/scripts.js
+++ oefening 13/js/scripts.js
@@ -1,1 +1,26 @@
 
+
+
+const calcBmi = () => {
+  let gewicht = parseFloat(prompt("wa weegde gij?"));
+  let grootte = parseFloat(prompt("hoe groot zijde gij? (in m)"))
+
+  let bmi = gewicht / grootte * grootte;
+
+  if (bmi < 16.5) {
+
+    alert(`BMI: ${bmi}\nondervoeding of hongersnood`);
+  } else if (16,5 <= bmi <18,5) {
+    alert(`BMI: ${bmi}\ndunheid`);
+  } else if (18,5 <= bmi <= BMI < 25) {
+    alert(`BMI: ${bmi}\nnormale bouw`);
+  } else if (25 <= bmi <30) {
+    alert(`BMI: ${bmi}\novergewicht`);
+  } else if (30 <= bmi <= bmi < 35) {
+    alert(`BMI: ${bmi}\nmatige overgewicht`);
+  } else if (35 <= bmi <40) {
+    alert(`BMI: ${bmi}\nernstige overgewicht`);
+  } else if (bmi > 40) {
+    alert(`BMI: ${bmi}\nmorbide obesitas`);
+  }
+};
